c110d105d353b5d191e5613cdc12f89f50732e7f
Lekzi <93331411+Lekzi@users.noreply.github.com>
2022-01-12 13:48:29 +0100

    Add files via upload

---
+++ CALENDAR.py
@@ -0,0 +1,312 @@
+import time as t
+import os
+t.sleep(5)
+x= "Enter a valid option!"
+os.system ("clear")
+print ("""CALENDAR is a python Calendar tool created by Lekzi""")
+t.sleep(8)
+os.system("clear")
+t.sleep(4)
+def loop():
+	os.system ("clear")
+	os.system (" figlet Lekzcal")
+	t.sleep(6)
+	print("""
+	Tool Name:CALENDAR
+	Creator:Lêkzï
+	Contact me on whatsapp:+2347064822519
+	Team:Greyhax0rs 
+	""")
+	print("""Special thanks to Spider Anongrey hat for his support""")
+loop()
+# importing calendar module
+import calendar
+
+yy = 2022 # year
+mm = 1 # month
+
+# To take month and year input from the user
+# yy = int(input("Enter year: "))
+# mm = int(input("Enter month: "))
+
+# display the calendar
+print(calendar.month(yy, mm))
+# importing calendar module
+import calendar
+
+yy = 2022 # year
+mm = 2 # month
+
+# To take month and year input from the user
+# yy = int(input("Enter year: "))
+# mm = int(input("Enter month: "))
+
+# display the calendar
+print(calendar.month(yy, mm))
+# importing calendar module
+import calendar
+
+yy = 2022 # year
+mm = 3 # month
+
+# To take month and year input from the user
+# yy = int(input("Enter year: "))
+# mm = int(input("Enter month: "))
+
+# display the calendar
+print(calendar.month(yy, mm))
+# importing calendar module
+import calendar
+
+yy = 2022 # year
+mm = 4 # month
+
+# To take month and year input from the user
+# yy = int(input("Enter year: "))
+# mm = int(input("Enter month: "))
+
+# display the calendar
+print(calendar.month(yy, mm))
+# importing calendar module
+import calendar
+
+yy = 2022 # year
+mm = 5 # month
+
+# To take month and year input from the user
+# yy = int(input("Enter year: "))
+# mm = int(input("Enter month: "))
+
+# display the calendar
+print(calendar.month(yy, mm))
+# importing calendar module
+import calendar
+
+yy = 2022 # year
+mm = 6 # month
+
+# To take month and year input from the user
+# yy = int(input("Enter year: "))
+# mm = int(input("Enter month: "))
+
+# display the calendar
+print(calendar.month(yy, mm))
+# importing calendar module
+import calendar
+
+yy = 2022 # year
+mm = 7 # month
+
+# To take month and year input from the user
+# yy = int(input("Enter year: "))
+# mm = int(input("Enter month: "))
+
+# display the calendar
+print(calendar.month(yy, mm))
+# importing calendar module
+import calendar
+
+yy = 2022 # year
+mm = 8 # month
+
+# To take month and year input from the user
+# yy = int(input("Enter year: "))
+# mm = int(input("Enter month: "))
+
+# display the calendar
+print(calendar.month(yy, mm))
+# importing calendar module
+import calendar
+
+yy = 2023 # year
+mm = 9 # month
+
+# To take month and year input from the user
+# yy = int(input("Enter year: "))
+# mm = int(input("Enter month: "))
+
+# display the calendar
+print(calendar.month(yy, mm))
+# importing calendar module
+import calendar
+
+yy = 2022 # year
+mm = 10 # month
+
+# To take month and year input from the user
+# yy = int(input("Enter year: "))
+# mm = int(input("Enter month: "))
+
+# display the calendar
+print(calendar.month(yy, mm))
+# importing calendar module
+import calendar
+
+yy = 2022 # year
+mm = 11 # month
+
+# To take month and year input from the user
+# yy = int(input("Enter year: "))
+# mm = int(input("Enter month: "))
+
+# display the calendar
+print(calendar.month(yy, mm))
+# importing calendar module
+import calendar
+ 
+yy = 2022 # year
+mm = 12 # month
+
+# To take month and year input from the user
+# yy = int(input("Enter year: "))
+# mm = int(input("Enter month: "))
+
+# display the calendar
+print(calendar.month(yy, mm))
+# importing calendar module
+
+# importing calendar module
+import calendar
+
+yy = 2023 # year
+mm = 1 # month
+
+# To take month and year input from the user
+# yy = int(input("Enter year: "))
+# mm = int(input("Enter month: "))
+
+# display the calendar
+print(calendar.month(yy, mm))
+# importing calendar module
+import calendar
+
+yy = 2023 # year
+mm = 2 # month
+
+# To take month and year input from the user
+# yy = int(input("Enter year: "))
+# mm = int(input("Enter month: "))
+
+# display the calendar
+print(calendar.month(yy, mm))
+# importing calendar module
+import calendar
+
+yy = 2023 # year
+mm = 3 # month
+
+# To take month and year input from the user
+# yy = int(input("Enter year: "))
+# mm = int(input("Enter month: "))
+
+# display the calendar
+print(calendar.month(yy, mm))
+# importing calendar module
+import calendar
+
+yy = 2023 # year
+mm = 4 # month
+
+# To take month and year input from the user
+# yy = int(input("Enter year: "))
+# mm = int(input("Enter month: "))
+
+# display the calendar
+print(calendar.month(yy, mm))
+# importing calendar module
+import calendar
+
+yy = 2023 # year
+mm = 5 # month
+
+# To take month and year input from the user
+# yy = int(input("Enter year: "))
+# mm = int(input("Enter month: "))
+
+# display the calendar
+print(calendar.month(yy, mm))
+# importing calendar module
+import calendar
+
+yy = 2023 # year
+mm = 6 # month
+
+# To take month and year input from the user
+# yy = int(input("Enter year: "))
+# mm = int(input("Enter month: "))
+
+# display the calendar
+print(calendar.month(yy, mm))
+# importing calendar module
+import calendar
+
+yy = 2023 # year
+mm = 7 # month
+
+# To take month and year input from the user
+# yy = int(input("Enter year: "))
+# mm = int(input("Enter month: "))
+
+# display the calendar
+print(calendar.month(yy, mm))
+# importing calendar module
+import calendar
+
+yy = 2023 # year
+mm = 8 # month
+
+# To take month and year input from the user
+# yy = int(input("Enter year: "))
+# mm = int(input("Enter month: "))
+
+# display the calendar
+print(calendar.month(yy, mm))
+# importing calendar module
+import calendar
+
+yy = 2023 # year
+mm = 9 # month
+
+# To take month and year input from the user
+# yy = int(input("Enter year: "))
+# mm = int(input("Enter month: "))
+
+# display the calendar
+print(calendar.month(yy, mm))
+# importing calendar module
+import calendar
+
+yy = 2023 # year
+mm = 10 # month
+
+# To take month and year input from the user
+# yy = int(input("Enter year: "))
+# mm = int(input("Enter month: "))
+
+# display the calendar
+print(calendar.month(yy, mm))
+# importing calendar module
+import calendar
+
+yy = 2023 # year
+mm = 11 # month
+
+# To take month and year input from the user
+# yy = int(input("Enter year: "))
+# mm = int(input("Enter month: "))
+
+# display the calendar
+print(calendar.month(yy, mm))
+# importing calendar module
+import calendar
+ 
+yy = 2023 # year
+mm = 12 # month
+# To take month and year input from the user
+# yy = int(input("Enter year: "))
+# mm = int(input("Enter month: "))
+
+# display the calendar
+print(calendar.month(yy, mm))
+# importing calendar module
+
